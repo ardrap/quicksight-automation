01ac7c3a88cc8be51921659b45d14e311d1fd7a2
Ardra P <74761969+ardrap@users.noreply.github.com>
2026-01-29 10:43:19 +0530

    Update Pulumidemo.sql
    demo4

--- Pulumidemo.sql
+++ Pulumidemo.sql
@@ -3,5 +3,6 @@ SELECT
     name AS "Brand Name",
     brand_code AS "Brand Code",
     country AS "Country",
-    is_active AS "Active"
+    is_active AS "Active",
+    order_number AS "number'
 FROM ygag_store_brand
