06590602b1c83396c31a324cc43827d91419bd4f
Ardra P <74761969+ardrap@users.noreply.github.com>
2026-01-29 10:19:11 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,27 +1,27 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
 import pulumi_aws as aws
 import os
 
-# 1. READ YOUR SQL FILE
+# Create the provider
+mumbai_provider = aws.Provider("mumbai", region="ap-south-1")
+
+# Read SQL
 sql_file_path = os.path.join(os.path.dirname(__file__), 'Pulumidemo.sql')
 with open(sql_file_path, 'r') as f:
     sql_query_content = f.read()
 
-# 2. DEFINE THE DATASET (This is the automation part)
-# This connects your local SQL file to the existing AWS Dataset
+# Define Dataset
 dataset = aws.quicksight.DataSet("qs-production-dataset",
     aws_account_id="261375936682",
     data_set_id="9beb65c3-c192-43b8-9b63-d543eef88159",
-    name="Pulumi_Test_Dataset", # The display name in QuickSight
+    name="Pulumi_Test_Dataset",
     import_mode="SPICE",
     physical_table_maps=[{
         "physical_table_map_id": "sql-table-01",
         "custom_sql": {
-            "data_source_arn": "arn:aws:quicksight:ap-south-1:261375936682:datasource/9eefb60c-73ac-40f9-8431-f171776c9c02", # Get this from 'aws quicksight list-data-sources'
+            "data_source_arn": "arn:aws:quicksight:ap-south-1:261375936682:datasource/9eefb60c-73ac-40f9-8431-f171776c9c02",
             "name": "Corporate_Brand_Query",
-            "sql_query": sql_query_content, # <--- THIS makes the magic happen
+            "sql_query": sql_query_content,
             "columns": [
                 { "name": "Corporate ID", "type": "INTEGER" },
                 { "name": "Brand Name", "type": "STRING" },
@@ -30,14 +30,17 @@ dataset = aws.quicksight.DataSet("qs-production-dataset",
         }
     }],
     logical_table_maps=[{
-        "logical_table_map_id": "sql-table-01", 
+        "logical_table_map_id": "sql-table-01",
         "alias": "Corporate_Brand_Query",
         "source": {
             "physical_table_id": "sql-table-01",
         },
     }],
-    # This tells Pulumi: "I already created this manually, please take control of it."
-    # opts=pulumi.ResourceOptions(import_="261375936682,9beb65c3-c192-43b8-9b63-d543eef88159")
+    opts=pulumi.ResourceOptions(
+        provider=mumbai_provider,
+        import_="261375936682,9beb65c3-c192-43b8-9b63-d543eef88159",
+        ignore_changes=["permissions"]
+    )
 )
 
 pulumi.export("dataset_arn", dataset.arn)
