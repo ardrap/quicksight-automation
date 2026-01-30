d50670d7df29df13c8311b7a1fb1a65f5bea6f87
Ardra P <74761969+ardrap@users.noreply.github.com>
2026-01-30 09:26:28 +0530

    Update Pulumidemo.sql

--- Pulumidemo.sql
+++ Pulumidemo.sql
@@ -4,6 +4,7 @@ SELECT
     brand_code AS "Brand Code",
     country AS "Country",
     is_active AS "Active",
-    is_luxury AS "Luxuary"
+    is_luxury AS "Luxuary",
+    retailer_id AS "Retailer ID"
     
 FROM ygag_store_brand
